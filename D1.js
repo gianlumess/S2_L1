6f1384e0747aa5b2b66b504c0ec006a1129d7fe3
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-05-06 16:10:45 +0200

    Esercizi js terminati

--- D1.js
+++ D1.js
@@ -11,38 +11,54 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*Per datatype si intende una classificazione della tipologia di dati. I principali datatype in javascript sono:
+-Stringhe: del testo delimitato dagli apici
+-Numeri: possono essere interi o decimali
+-Booleani: Possono avere solamente 2 valori: SI o NO (TRUE OR FALSE)
+-Null: Un valore nullo, come una scatola vuota(non è uguale a 0!!)
+-undefined: significa che non vi è un valore specifico perchè non è stato effettivamente asseganto
+ */
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let myName="Gianluca";
+console.log("il valore della variabile myName è: ", myName)
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let addizione= 12+20;
+console.log("12+20 è uguale ad: ", addizione)
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let x= 12;
+console.log("il valore della variabile x è: ", x)
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+myName= "Messina";
+console.log("il valore della variabile myName adesso è: ", myName)
+/*
+const costante= "ciao";    non è possibile cambiare valore ad una variabile "const" in quanto è un valore fisso e l'errore viene riportato sulla console.
+costante= "arrivederci";
+*/
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let sottrazione = 4-x;
+console.log("4- la variabile x è uguale ad: ", sottrazione)
+
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +66,9 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let name1= "john";
+let name2= "John";
+console.log(name1 !== name2);
+ /*EXTRA */
+ name2= name2.toLowerCase();
+ console.log(name1 === name2)
